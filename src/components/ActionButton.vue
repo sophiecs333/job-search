c046c5b66da1942a4c87911850d6cfede6f7a452
sophie sorin <sophie4489@hotmail.fr>
2022-07-05 16:36:05 +0200

    adding v-on:click @

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,7 +1,7 @@
 <template>
   <button
     class="px-5 py-3 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-    v-on:click="handleClick"
+    @click="handleClick"
   >
     Sign In
   </button>
