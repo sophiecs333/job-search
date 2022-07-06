2ef51e106ab9942c65d8a5873e0594955f993fcb
sophie sorin <sophie4489@hotmail.fr>
2022-07-06 12:52:57 +0200

    starting subnav styling

--- src/components/Subnav.vue
+++ src/components/Subnav.vue
@@ -1,10 +1,11 @@
 <template>
-<div>This is a subnav</div
+  <div class="w-full h-16 bg-white border-b border-solid border-brand-gray-1">
+    <div class="flex items-center h-full px-8">This is a subnav</div>
+  </div>
 </template>
 
 <script>
-
 export default {
-  name: "Subnav"
+  name: "Subnav",
 };
 </script>
