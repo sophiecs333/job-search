216b7b3f53fb910bc68cdef04e1b7a851aaaf9c6
sophie sorin <sophie4489@hotmail.fr>
2022-07-07 11:02:44 +0200

    applying css grid to hero

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,6 +1,21 @@
 <template>
   <main>
-    <section>Hero section</section>
+    <section class="flex flex-col h-screen pt-10 pb-20 bg-yellow-200">
+      <div class="grid grid-cols-12">
+        <div class="col-start-1 col-span-1 border border-blue-700">
+          1 column
+        </div>
+        <div class="col-start-2 col-span-5 border border-blue-700">
+          5 columns
+        </div>
+        <div class="col-start-7 col-span-5 border border-blue-700">
+          5 columns
+        </div>
+        <div class="col-start-12 col-span-1 border border-blue-700">
+          1 column
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
