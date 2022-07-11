f240595128a5d161a42593482da578a1f3da45aa
sophie sorin <sophie4489@hotmail.fr>
2022-07-11 10:02:02 +0200

    refactoring + TextInput test

--- src/components/jobsearch/JobSearchForm.vue
+++ src/components/jobsearch/JobSearchForm.vue
@@ -6,10 +6,7 @@
     <div class="flex flex-nowrap flex-1 h-full text-base font-light">
       <div class="relative flex items-center flex-1 h-full pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <text-input
-          placeholder="Software Engineer"
-          @handle-input="updateRole"
-        />
+        <text-input v-model="role" placeholder="Software Engineer" />
       </div>
 
       <span
@@ -19,7 +16,7 @@
 
       <div class="relative flex items-center flex-1 h-full pl-3">
         <label class="absolute left-0 -top-10">Where</label>
-        <text-input placeholder="Los Angeles" @handle-input="updateLocation" />
+        <text-input v-model="location" placeholder="Los Angeles" />
       </div>
     </div>
     <action-button text="Search" type="secondary" class="rounded-r-3xl" />
--- src/components/shared/TextInput.vue
+++ src/components/shared/TextInput.vue
@@ -1,7 +1,7 @@
 <template>
   <input
     type="text"
-    :value="value"
+    :value="modelValue"
     :placeholder="placeholder"
     class="w-full text-lg font-normal focus:outline-none"
     @input="handleInput"
@@ -9,26 +9,25 @@
 </template>
 
 <script>
-import { stringify } from 'postcss';
+import { stringify } from "postcss";
 export default {
   name: "TextInput",
   props: {
     placeholder: {
       type: String,
       required: false,
-      default: ""
-    }
-  },
-  data() {
-    return {
-      value:
-    }
+      default: "",
+    },
+    modelValue: {
+      type: String,
+      required: true,
+    },
   },
   methods: {
     handleInput($event) {
-      this.value = $event.target.value
-      this.$emit("handleInput", this.value)
-    }
-  }
+      this.value = $event.target.value;
+      this.$emit("update:modelValue", $event.target.value);
+    },
+  },
 };
 </script>
