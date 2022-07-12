c4b946ed3a9b60d1eace8a6859dcec14e9817304
sophie sorin <sophie4489@hotmail.fr>
2022-07-12 09:13:25 +0200

    routing to Home

--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,9 +1,17 @@
 <template>
-  <h1 class="text-6xl font-bold">Job Results View</h1>
+  <div>
+    <h1 class="text-6xl font-bold">Job Results View</h1>
+    <button @click="goToHome">Home</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "JobResultsView",
+  methods: {
+    goToHome() {
+      this.$route.push({ name: "Home" });
+    },
+  },
 };
 </script>
