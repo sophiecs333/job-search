fdcd204402bbcaa8f746d90ff4b5ce707d0c6683
sophie sorin <sophie4489@hotmail.fr>
2022-07-12 09:26:03 +0200

    updating MainNav with router-link

--- src/components/navigation/MainNav.vue
+++ src/components/navigation/MainNav.vue
@@ -4,9 +4,11 @@
       <div
         class="flex flex-nowrap h-full px-8 mx-auto border-b border-solid border-brand-gray-1"
       >
-        <a :href="url" class="flex items-center h-full text-xl">{{
-          company
-        }}</a>
+        <router-link
+          :to="{ name: 'Home' }"
+          class="flex items-center h-full text-xl"
+          >Poogle Careers</router-link
+        >
 
         <nav class="h-full ml-12">
           <ul class="flex h-full p-0 m-0 list-none">
@@ -53,8 +55,6 @@ export default {
   },
   data() {
     return {
-      company: "Poogle Careers",
-      url: "https://careers.google.com",
       menuItems: [
         "Teams",
         "Locations",
