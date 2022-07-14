87dccef4e560cf5c2028a35d88c424219276b24d
sophie sorin <sophie4489@hotmail.fr>
2022-07-14 08:42:08 +0200

    implementing first job page

---
+++ src/components/JobResults/JobListings.vue
@@ -0,0 +1,57 @@
+<template>
+  <main class="flex-auto p-8 bg-brand-gray-2">
+    <ol>
+      <li class="mb-7">
+        <router-link
+          to="/jobs/results/1"
+          class="block mx-auto bg-white border border-solid border-brand-gray-2 rounded hover:shadow-gray"
+        >
+          <div class="pt-5 pb-2 mx-8 border-b border-solid border-brand-gray-2">
+            <h2 class="mb-2 text-2xl">
+              Technical Program Manager, Perception, Augmented Reality
+            </h2>
+
+            <div class="flex flex-row align-middle">
+              <div class="mr-5">
+                <span>Bobo</span>
+              </div>
+
+              <div>
+                <span>San Francisco, CA, USA</span>
+              </div>
+            </div>
+          </div>
+
+          <div class="px-8 py-4">
+            <div>
+              <h3 class="mt-1 mb-2">Qualifications</h3>
+              <div>
+                <ul class="pl-8 list-disc">
+                  <li>Bachelor's degree or equivalent practice experience.</li>
+                  <li>5 years of experience in program management.</li>
+                  <li>
+                    Experience analyzng data through querying database (e.g.
+                    SQL), using spreadsheet software, and creating statistical
+                    models
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <div class="mt-2 text-center">
+              <router-link to="/jobs/results/1" class="text-brand-blue-1"
+                >Expand</router-link
+              >
+            </div>
+          </div>
+        </router-link>
+      </li>
+    </ol>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "JobListings",
+};
+</script>
--- src/views/JobResultsView.vue
+++ src/views/JobResultsView.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
-    <h1 class="text-6xl font-bold">Job Results View</h1>
-    <button @click="goToHome">Home</button>
+    <job-filters-sidebar />
+    <job-listings />
   </div>
 </template>
 
 <script>
+import JobFiltersSidebar from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue";
+import JobListings from "@/components/JobResults/JobListings.vue";
+
 export default {
   name: "JobResultsView",
-  methods: {
-    goToHome() {
-      this.$router.push({ name: "Home" });
-    },
+  components: {
+    JobFiltersSidebar,
+    JobListings,
   },
 };
 </script>
