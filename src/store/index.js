e6f7de2c4338aae608d772306a0ba2321ef2bdfa
sophie sorin <sophie4489@hotmail.fr>
2022-07-18 15:06:20 +0200

    vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,21 @@
 import { createStore } from "vuex";
 
-export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+export const state = () => {
+  return {
+    isLoggedIn: false,
+  };
+};
+
+export const mutations = {
+  LOGIN_USER(state) {
+    state.isLoggedIn = true;
+  },
+};
+
+const store = createStore({
+  state,
+  mutations,
+  strict: process.env.NODE_ENV !== "production",
 });
+
+export default store;
