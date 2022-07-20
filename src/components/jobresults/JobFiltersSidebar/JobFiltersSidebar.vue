ace50811203632cd5658d63d56917921215f8672
sophie sorin <sophie4489@hotmail.fr>
2022-07-20 21:58:59 +0200

    first checkbox

--- src/components/jobresults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/jobresults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -10,8 +10,18 @@
         </div>
       </div>
 
-      <accordion>Custom Content </accordion>
-      <accordion>Custom Graou </accordion>
+      <accordion>
+        <div class="mt-5">
+          <fieldset>
+            <ul class="flex flex-row flex-wrap">
+              <li class="w-1/2 h-8">
+                <input id="VueTube" type="checkbox" class="mr-3" />
+                <label for="VueTube">VueTube</label>
+              </li>
+            </ul>
+          </fieldset>
+        </div>
+      </accordion>
     </section>
   </div>
 </template>
