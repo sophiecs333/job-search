547485400bee2da9f4915e1a27238f6c82d96542
sophie sorin <sophie4489@hotmail.fr>
2022-07-20 19:06:19 +0200

    sidebar CSS

--- src/components/jobresults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ src/components/jobresults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -2,12 +2,24 @@
   <div
     class="flex flex-col p-4 bg-white border-r border-solid border-brand-gray-1 w-96"
   >
-    Job Filters Sidebar
+    <section class="pb-5">
+      <div class="flex flex-row justify-between">
+        <h3 class="my-4 text-base font-semibold">What do you want to do?</h3>
+        <div class="flex items-center text-sm">
+          <action-button text="Clear filters" type="secondary" />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import ActionButton from "@/components/Shared/ActionButton.vue";
+
 export default {
   name: "JobFiltersSidebar",
+  components: {
+    ActionButton,
+  },
 };
 </script>
