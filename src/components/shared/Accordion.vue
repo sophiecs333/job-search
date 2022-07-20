6da0eaa6924c1ed1cbd4149de86b6651cbe447ff
sophie sorin <sophie4489@hotmail.fr>
2022-07-20 21:27:56 +0200

    more style for accordion

--- src/components/shared/Accordion.vue
+++ src/components/shared/Accordion.vue
@@ -1,9 +1,35 @@
 <template>
-  <div>My accordion</div>
+  <div class="py-5 border-b border-solid border-brand-gray-2">
+    <div
+      class="flex flex-wrap items-center justify-between cursor-pointer"
+      data-test="clickable-area"
+      @click="open"
+    >
+      <h3 class="text-base font-semibold">{{ header }}</h3>
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+
+    <div v-if="isOpen" class="w-full mt-5"></div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Accordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
