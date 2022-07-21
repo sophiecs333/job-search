5008a58c92b266a967453360c0a730eb891689f5
sophie sorin <sophie4489@hotmail.fr>
2022-07-21 19:56:52 +0200

    scoped slots and props

--- src/components/jobsearch/Hero.vue
+++ src/components/jobsearch/Hero.vue
@@ -17,20 +17,20 @@
       </div>
     </section>
     <spotlight class="flex flex-row justify-center pb-16">
-      <template #default="slotProps">
+      <template #default="{ img, title, description }">
         <router-link
           to="/jobs/results"
           class="flex flex-col mx-5 border rounded-lg w-72 bg-brand-gray-2 h-96"
         >
-          <img :src="slotProps.spotlight.img" class="object-contain" />
+          <img :src="img" class="object-contain" />
 
           <div class="h-48 px-6 py-4 mt-3">
             <h3 class="text-lg font-medium">
-              {{ slotProps.spotlight.title }}
+              {{ title }}
             </h3>
 
             <p class="mt-3 text-sm">
-              {{ slotProps.spotlight.description }}
+              {{ description }}
             </p>
           </div>
 
