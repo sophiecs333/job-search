51402f3966480d6af9ff6c4ba770447254df2370
sophie sorin <sophie4489@hotmail.fr>
2022-07-22 11:31:12 +0200

    getters

--- src/store/index.js
+++ src/store/index.js
@@ -6,6 +6,8 @@ export const LOGIN_USER = "LOGIN_USER";
 export const RECEIVE_JOBS = "RECEIVE_JOBS";
 export const FETCH_JOBS = "FETCH_JOBS";
 
+export const UNIQUE_ORGANIZATIONS = "UNIQUE_ORGANIZATIONS";
+
 export const state = () => {
   return {
     isLoggedIn: false,
@@ -19,6 +21,14 @@ export const mutations = {
   },
   [RECEIVE_JOBS](state, jobs) {
     state.jobs = jobs;
+  },
+};
+
+export const getters = {
+  [UNIQUE_ORGANIZATIONS](state) {
+    const uniqueOrganizations = new Set();
+    state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
+    return uniqueOrganizations;
   },
 };
 
