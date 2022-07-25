8bf6e94aa3eef4fa06416a0d8455e3b82b0806a4
sophie sorin <sophie4489@hotmail.fr>
2022-07-25 08:08:53 +0200

    getter in index

--- src/store/index.js
+++ src/store/index.js
@@ -36,6 +36,13 @@ export const getters = {
     state.jobs.forEach((job) => uniqueOrganizations.add(job.organization));
     return uniqueOrganizations;
   },
+  [FILTERED_JOBS_BY_ORGANIZATIONS](state) {
+    if (state.selectedOrganizations.length === 0) return state.jobs;
+
+    return state.jobs.filter((job) =>
+      state.selectedOrganizations.includes(job.organization)
+    );
+  },
 };
 
 export const actions = {
