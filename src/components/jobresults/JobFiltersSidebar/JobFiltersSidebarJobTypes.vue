9157d24177d47d64d7d0552a679d7ead6ae0813f
sophie sorin <sophie4489@hotmail.fr>
2022-08-03 14:53:00 +0200

    more refactor

--- src/components/jobresults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
+++ src/components/jobresults/JobFiltersSidebar/JobFiltersSidebarJobTypes.vue
@@ -4,7 +4,7 @@
       <fieldset>
         <ul class="flex flex-row flex-wrap">
           <li
-            v-for="jobType in UNIQUE_JOB_TYPES"
+            v-for="jobType in uniqueJobTypes"
             :key="jobType"
             class="w-1/2 h-8"
           >
@@ -26,9 +26,12 @@
 </template>
 
 <script>
-import { mapGetters, mapMutations } from "vuex";
+import { ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
-import { UNIQUE_JOB_TYPES, ADD_SELECTED_JOB_TYPES } from "@/store/constants";
+import { useUniqueJobTypes } from "@/store/composables";
+import { ADD_SELECTED_JOB_TYPES } from "@/store/constants";
 
 import Accordion from "@/components/shared/Accordion.vue";
 
@@ -37,20 +40,34 @@ export default {
   components: {
     Accordion,
   },
-  data() {
-    return {
-      selectedJobTypes: [],
+  setup() {
+    const store = useStore();
+    const router = useRouter();
+
+    const selectedJobTypes = ref([]);
+    const uniqueJobTypes = useUniqueJobTypes();
+
+    const selectJobType = () => {
+      store.commit(ADD_SELECTED_JOB_TYPES, selectedJobTypes.value);
+      router.push({ name: "JobResults" });
     };
+
+    return { selectedJobTypes, uniqueJobTypes, selectJobType };
   },
-  computed: {
-    ...mapGetters([UNIQUE_JOB_TYPES]),
-  },
-  methods: {
-    ...mapMutations([ADD_SELECTED_JOB_TYPES]),
-    selectJobType() {
-      this.ADD_SELECTED_JOB_TYPES(this.selectedJobTypes);
-      this.$router.push({ name: "JobResults" });
-    },
-  },
+  // data() {
+  //   return {
+  //     selectedJobTypes: [],
+  //   };
+  // },
+  // computed: {
+  //   ...mapGetters([UNIQUE_JOB_TYPES]),
+  // },
+  // methods: {
+  //   ...mapMutations([ADD_SELECTED_JOB_TYPES]),
+  //   selectJobType() {
+  //     this.ADD_SELECTED_JOB_TYPES(this.selectedJobTypes);
+  //     this.$router.push({ name: "JobResults" });
+  //   },
+  // },
 };
 </script>
--- tests/unit/store/composables.test.js
+++ tests/unit/store/composables.test.js
@@ -1,8 +1,12 @@
 import { useStore } from "vuex";
 jest.mock("vuex");
 
-import { useFilteredJobs } from "@/store/composables";
-import { FILTERED_JOBS } from "@/store/constants";
+import {
+  useFilteredJobs,
+  useUniqueJobTypes,
+  useUniqueOrganizations,
+} from "@/store/composables";
+// import { FILTERED_JOBS } from "@/store/constants";
 
 describe("composables", () => {
   describe("useFilteredJobs", () => {
@@ -15,6 +19,32 @@ describe("composables", () => {
 
       const result = useFilteredJobs();
       expect(result.value).toEqual([{ id: 1 }]);
+    });
+  });
+
+  describe("useUniqueJobTypes", () => {
+    it("retrieves unique job types from store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_JOB_TYPES: new Set(["Full-time"]),
+        },
+      });
+
+      const result = useUniqueJobTypes();
+      expect(result.value).toEqual(new Set(["Full-time"]));
+    });
+  });
+
+  describe("useUniqueOrganizations", () => {
+    it("retrieves unique job organizations from store", () => {
+      useStore.mockReturnValue({
+        getters: {
+          UNIQUE_ORGANIZATIONS: new Set(["Apple"]),
+        },
+      });
+
+      const result = useUniqueOrganizations();
+      expect(result.value).toEqual(new Set(["Apple"]));
     });
   });
 });
