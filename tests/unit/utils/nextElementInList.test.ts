07d2d4643d8702255341f0da6e4c70a8423931ab
sophie sorin <sophie4489@hotmail.fr>
2022-08-21 18:03:17 +0200

    nextElementInList to ts

--- tests/unit/utils/nextElementInList.test.ts
+++ tests/unit/utils/nextElementInList.test.ts
@@ -4,7 +4,7 @@ describe("nextElementInList", () => {
   it("locates element in list and returns next element in list", () => {
     const list = ["A", "B", "C", "D", "E"];
     const value = "C";
-    const result = nextElementInList(list, value);
+    const result = nextElementInList<string>(list, value);
     expect(result).toBe("D");
   });
 
@@ -12,7 +12,7 @@ describe("nextElementInList", () => {
     it("locates next element at start of the list", () => {
       const list = ["A", "B", "C", "D", "E"];
       const value = "E";
-      const result = nextElementInList(list, value);
+      const result = nextElementInList<string>(list, value);
       expect(result).toBe("A");
     });
   });
